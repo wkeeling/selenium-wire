f6f83d08d91af84418daa64e6355cf1b7dc0eceb
Will Keeling <will@zifferent.com>
2018-07-11 22:34:00 +0100

    Parse proxy URL

--- seleniumwire/proxy/server.py
+++ seleniumwire/proxy/server.py
@@ -1,3 +1,5 @@
+from urllib.request import _parse_proxy
+
 from .proxy2 import ThreadingHTTPServer
 from .storage import RequestStorage
 from .util import RequestModifier
@@ -8,6 +10,11 @@ class ProxyHTTPServer(ThreadingHTTPServer):
     def __init__(self, options, *args, **kwargs):
         # The seleniumwire options
         self.options = options
+        try:
+            # Parse the proxy URL into (proxy_type, user, password, hostport)
+            self.options['proxy'] = _parse_proxy(options['proxy'])
+        except KeyError:
+            pass
         # Each server instance gets its own storage
         self.storage = RequestStorage()
         # Each server instance gets a request modifier
