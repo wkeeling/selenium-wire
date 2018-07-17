f796a94d1b6c5bf7d777ca7a0ffae119b75a7f08
Will Keeling <will@zifferent.com>
2018-07-17 22:29:32 +0100

    Further work on acceptance test

--- tests/acceptance.py
+++ tests/acceptance.py
@@ -17,11 +17,6 @@ from seleniumwire import webdriver
 
 class SeleniumIntegrationTest(TestCase):
 
-    driver_config = [
-        ('https://www.wikipedia.org/', 'Wikipedia'),
-        ('https://github.com/', 'GitHub')
-    ]
-
     options = {
             'proxy': {
                 'http': 'http://167.99.193.171:8000',
@@ -31,14 +26,55 @@ class SeleniumIntegrationTest(TestCase):
 
     def test_firefox_can_access_requests(self):
         url = 'https://www.python.org/'
-        title = 'Python'
-        driver = webdriver.Firefox(seleniumwire_options=self.options)
+        driver = webdriver.Firefox()
         driver.get(url)
 
-        self.assertTrue(title in driver.title)
         request = driver.wait_for_request(url)
+
         self.assertEqual(request.response.status_code, 200)
         self.assertIn('text/html', request.response.headers['Content-Type'])
 
         driver.quit()
 
+    def test_chrome_can_access_requests(self):
+        url = 'https://www.wikipedia.org/'
+        driver = webdriver.Chrome()
+        driver.get(url)
+
+        request = driver.wait_for_request(url)
+
+        self.assertEqual(request.response.status_code, 200)
+        self.assertIn('text/html', request.response.headers['Content-Type'])
+
+        driver.quit()
+
+    def test_safari_can_access_requests(self):
+        url = 'https://www.stackoverflow.com/'
+        options = {
+            'port': 12345
+        }
+        driver = webdriver.Safari(seleniumwire_options=options)
+        driver.get(url)
+
+        request = driver.wait_for_request(url)
+
+        self.assertEqual(request.response.status_code, 200)
+        self.assertIn('text/html', request.response.headers['Content-Type'])
+
+        driver.quit()
+
+    def test_modify_user_agent(self):
+        user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) ' \
+                     'Chrome/28.0.1500.52 Safari/537.36 OPR/15.0.1147.100'
+        url = 'https://www.python.org/'
+        driver = webdriver.Firefox()
+        driver.header_overrides = {
+            'User-Agent': user_agent
+        }
+        driver.get(url)
+
+        request = driver.wait_for_request(url)
+
+        self.assertEqual(request.headers['User-Agent'], user_agent)
+
+        driver.quit()
