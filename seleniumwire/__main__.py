9c81a99d62a012b7530aeb01dd2eecef5fe368e2
Will Keeling <will@zifferent.com>
2018-07-17 17:48:32 +0100

    Support passing args from command line

--- seleniumwire/__main__.py
+++ seleniumwire/__main__.py
@@ -8,7 +8,8 @@ from seleniumwire.proxy import client, util
 logging.basicConfig(level=logging.DEBUG, format='%(message)s')
 
 
-def standalone_proxy():
+def standalone_proxy(port):
+    port = int(port)
     http_proxy = os.environ.get('http_proxy')
     https_proxy = os.environ.get('https_proxy')
     no_proxy = os.environ.get('no_proxy')
@@ -23,7 +24,7 @@ def standalone_proxy():
         proxy_config['no_proxy'] = no_proxy
 
     c = client.AdminClient()
-    c.create_proxy(proxy_config=proxy_config, standalone=True)
+    c.create_proxy(port=port, proxy_config=proxy_config, standalone=True)
 
 
 if __name__ == '__main__':
@@ -36,10 +37,20 @@ if __name__ == '__main__':
                                      formatter_class=RawDescriptionHelpFormatter,
                                      usage='python -m seleniumwire <command>')
     parser.add_argument('command', help='The command name')
+    parser.add_argument('args', nargs='*', help='Optional list of space separated positional and keyword arguments, '
+                                                'e.g. arg1 arg2 kwarg1=12345', default=None)
 
     args = parser.parse_args()
+    pargs = [arg for arg in args.args if '=' not in arg and arg is not args.command]
+    kwargs = dict([tuple(arg.split('=')) for arg in args.args if '=' in arg])
 
     try:
-        commands[args.command]()
+        commands[args.command](*pargs, **kwargs)
     except KeyError:
         print("Unsupported command '{}' (use --help for list of commands)".format(args.command))
+    except TypeError as e:
+        if 'unexpected' in str(e):
+            print('Unrecognised arguments: {} {}'.format(' '.join(pargs), ' '.join('{}={}'.format(k, v)
+                                                                                   for k, v in kwargs.items())))
+        elif 'missing' in str(e):
+            print('Missing arguments')
