08a893af2b63738669e65105da225b1f139f5c1d
Will Keeling <will@zifferent.com>
2018-07-24 13:37:38 +0100

    Tidy

--- setup.py
+++ setup.py
@@ -11,12 +11,6 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ ]
-
-setup_requirements = [ ]
-
-test_requirements = [ ]
-
 setup(
     author="Will Keeling",
     author_email='will@zifferent.com',
