2d0b483c4fecd5e9221e09e4857c508cfa76242a
Will Keeling <will@zifferent.com>
2018-08-01 21:47:03 +0100

    Remove Python 3 classifier

--- setup.py
+++ setup.py
@@ -19,7 +19,6 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
