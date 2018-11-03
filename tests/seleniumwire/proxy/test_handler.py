c9e60e65764e62ba04250c326005cdb2810e686b
Will Keeling <will@zifferent.com>
2018-11-03 10:56:21 +0000

    Add AdminMixinTest

--- tests/seleniumwire/proxy/test_handler.py
+++ tests/seleniumwire/proxy/test_handler.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from unittest.mock import Mock
+from unittest.mock import call, Mock
 
 from seleniumwire.proxy.handler import CaptureRequestHandler
 
@@ -61,4 +61,156 @@ class CaptureRequestHandlerTest(TestCase):
         self.handler.server.options = {}
         self.handler.path = 'https://www.google.com/foo/bar?x=y'
         self.handler.command = 'GET'
-        self.body = b'hello world'
+        self.body = None
+
+
+class AdminMixinTest(TestCase):
+
+    def test_get_requests(self):
+        self.handler.path = 'http://seleniumwire/requests'
+        self.mock_storage.load_requests.return_value = [
+            {'id': '12345'},
+            {'id': '67890'},
+        ]
+
+        self.handler.admin_handler()
+
+        self.mock_storage.load_requests.assert_called_once()
+        self.assert_response_mocks_called(
+            status=200,
+            headers=[('Content-Type', 'application/json'),
+                     ('Content-Length', 34)],
+            body=b'[{"id": "12345"}, {"id": "67890"}]'
+        )
+
+    def test_delete_requests(self):
+        self.handler.path = 'http://seleniumwire/requests'
+        self.handler.command = 'DELETE'
+
+        self.handler.admin_handler()
+
+        self.mock_storage.clear_requests.assert_called_once()
+
+        self.assert_response_mocks_called(
+            status=200,
+            headers=[('Content-Type', 'application/json'),
+                     ('Content-Length', 16)],
+            body=b'{"status": "ok"}'
+        )
+
+    def test_get_last_request(self):
+        self.handler.path = 'http://seleniumwire/last_request'
+        self.mock_storage.load_last_request.return_value = {'id': '12345'}
+
+        self.handler.admin_handler()
+
+        self.mock_storage.load_last_request.assert_called_once()
+
+        self.assert_response_mocks_called(
+            status=200,
+            headers=[('Content-Type', 'application/json'),
+                     ('Content-Length', 15)],
+            body=b'{"id": "12345"}'
+        )
+
+    def test_get_request_body(self):
+        self.handler.path = 'http://seleniumwire/request_body?request_id=12345'
+        self.mock_storage.load_request_body.return_value = b'bodycontent'
+
+        self.handler.admin_handler()
+
+        self.mock_storage.load_request_body.assert_called_once_with('12345')
+        self.assert_response_mocks_called(
+            status=200,
+            headers=[('Content-Type', 'application/octet-stream'),
+                     ('Content-Length', 11)],
+            body=b'bodycontent'
+        )
+
+    def test_get_response_body(self):
+        self.handler.path = 'http://seleniumwire/response_body?request_id=12345'
+        self.mock_storage.load_response_body.return_value = b'bodycontent'
+
+        self.handler.admin_handler()
+
+        self.mock_storage.load_response_body.assert_called_once_with('12345')
+        self.assert_response_mocks_called(
+            status=200,
+            headers=[('Content-Type', 'application/octet-stream'),
+                     ('Content-Length', 11)],
+            body=b'bodycontent'
+        )
+
+    def test_get_response_body_string(self):
+        self.handler.path = 'http://seleniumwire/response_body?request_id=12345'
+        self.mock_storage.load_response_body.return_value = 'bodycontent'
+
+        self.handler.admin_handler()
+
+        self.mock_storage.load_response_body.assert_called_once_with('12345')
+        self.assert_response_mocks_called(
+            status=200,
+            headers=[('Content-Type', 'application/octet-stream'),
+                     ('Content-Length', 11)],
+            body=b'bodycontent'
+        )
+
+    def test_find(self):
+        self.handler.path = 'http://seleniumwire/find?path=/foo/bar'
+        self.mock_storage.find.return_value = {'id': '12345'}
+
+        self.handler.admin_handler()
+
+        self.mock_storage.find.assert_called_once_with('/foo/bar')
+        self.assert_response_mocks_called(
+            status=200,
+            headers=[('Content-Type', 'application/json'),
+                     ('Content-Length', 15)],
+            body=b'{"id": "12345"}'
+        )
+
+    def test_find_no_match(self):
+        self.handler.path = 'http://seleniumwire/find?path=/foo/bar'
+        self.mock_storage.find.return_value = None
+
+        self.handler.admin_handler()
+
+        self.mock_storage.find.assert_called_once_with('/foo/bar')
+        self.assert_response_mocks_called(
+            status=200,
+            headers=[('Content-Type', 'application/json'),
+                     ('Content-Length', 4)],
+            body=b'null'
+        )
+
+    def test_set_header_overrides(self):
+        self.handler.path = 'http://seleniumwire/header_overrides'
+        self.command = 'POST'
+
+
+
+    def assert_response_mocks_called(self, status, headers, body):
+        self.mock_send_response.assert_called_once_with(status)
+        self.mock_send_header.assert_has_calls([call(k, v) for k, v in headers])
+        self.mock_end_headers.assert_called_once()
+        self.mock_wfile.write.assert_called_once_with(body)
+
+    def setUp(self):
+        CaptureRequestHandler.__init__ = Mock(return_value=None)
+        self.mock_modifier = Mock()
+        self.mock_storage = Mock()
+        self.mock_send_response = Mock()
+        self.mock_send_header = Mock()
+        self.mock_end_headers = Mock()
+        self.mock_wfile = Mock()
+
+        self.handler = CaptureRequestHandler()
+        self.handler.server = Mock()
+        self.handler.server.modifier = self.mock_modifier
+        self.handler.server.storage = self.mock_storage
+        self.handler.server.options = {}
+        self.handler.command = 'GET'
+        self.handler.send_response = self.mock_send_response
+        self.handler.send_header = self.mock_send_header
+        self.handler.end_headers = self.mock_end_headers
+        self.handler.wfile = self.mock_wfile
