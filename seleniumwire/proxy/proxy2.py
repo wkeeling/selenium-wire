130524f73549a9104f7204e0f7be5ab3560f2880
Will Keeling <will@zifferent.com>
2019-06-14 21:32:35 +0100

    Connection should be closed once we are done processing

--- seleniumwire/proxy/proxy2.py
+++ seleniumwire/proxy/proxy2.py
@@ -140,6 +140,7 @@ class ProxyRequestHandler(BaseHTTPRequestHandler):
         if res_body:
             self.wfile.write(res_body)
         self.wfile.flush()
+        self.close_connection = True
 
     def create_connection(self, origin):
         scheme, netloc = origin
