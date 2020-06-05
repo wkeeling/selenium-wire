3a97a1338b2212c52dcdb5688d77c7e015a2d896
Will Keeling <will.keeling@nanoporetech.com>
2020-06-05 14:43:51 +0100

    Introduce BoundedThreadingMixin

--- seleniumwire/proxy/server.py
+++ seleniumwire/proxy/server.py
@@ -2,6 +2,7 @@ import os
 import socket
 import ssl
 import sys
+import threading
 from collections import namedtuple
 from http.server import HTTPServer
 from socketserver import ThreadingMixIn
@@ -11,7 +12,30 @@ from .modifier import RequestModifier
 from .storage import RequestStorage
 
 
-class ProxyHTTPServer(ThreadingMixIn, HTTPServer):
+class BoundedThreadingMixin(ThreadingMixIn):
+    """Mix-in class that sets a maximum number of threads to handle requests."""
+
+    def __init__(self, max_threads, *args, **kwargs):
+        self.sema = threading.BoundedSemaphore(value=max_threads)
+        super().__init__(*args, **kwargs)
+
+    def process_request_thread(self, request, client_address):
+        super().process_request_thread(request, client_address)
+        self.sema.release()
+
+    def process_request(self, request, client_address):
+        t = threading.Thread(target=self.process_request_thread,
+                             args=(request, client_address))
+        t.daemon = self.daemon_threads
+        if not t.daemon and self._block_on_close:
+            if self._threads is None:
+                self._threads = []
+            self._threads.append(t)
+        self.sema.acquire()
+        t.start()
+
+
+class ProxyHTTPServer(BoundedThreadingMixin, HTTPServer):
     address_family = socket.AF_INET
     daemon_threads = True
 
@@ -34,7 +58,7 @@ class ProxyHTTPServer(ThreadingMixIn, HTTPServer):
         # The scope of requests we're interested in capturing.
         self.scopes = []
 
-        super().__init__(*args, **kwargs)
+        super().__init__(options.get('max_threads', 9999), *args, **kwargs)
 
     def _merge_with_env(self, proxy_config):
         """Merge upstream proxy configuration with configuration loaded
