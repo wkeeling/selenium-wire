71699cda48996c6c33fa7007548b5cb6b7c38e3c
Will Keeling <will@zifferent.com>
2021-01-30 10:38:37 +0000

    Fix acceptance tests

--- tests/acceptance.py
+++ tests/acceptance.py
@@ -24,18 +24,18 @@ class BrowserIntegrationTest(TestCase):
                     request.response.headers['Content-Type']
                 )
 
+        driver.quit()
+
     def test_firefox_can_access_requests(self):
         url = 'https://www.python.org/'
         driver = webdriver.Firefox()
         driver.get(url)
 
-        # request = driver.wait_for_request(url)
-        #
-        # self.assertEqual(request.response.status_code, 200)
-        # self.assertIn('text/html', request.response.headers['Content-Type'])
+        request = driver.wait_for_request(url)
 
-        import time
-        time.sleep(600)
+        self.assertEqual(request.response.status_code, 200)
+        self.assertIn('text/html', request.response.headers['Content-Type'])
+
         driver.quit()
 
     def test_firefox_can_access_requests_mitmproxy(self):
@@ -52,6 +52,25 @@ class BrowserIntegrationTest(TestCase):
         self.assertIn('text/html', request.response.headers['Content-Type'])
 
         driver.quit()
+
+    def test_referer(self):
+        import subprocess
+        import time
+
+        def interceptor(request):
+            del request.headers['referer']
+            request.headers['referer'] = "https://www.test.com"
+
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option("debuggerAddress", "127.0.0.1:12264")
+        chromeadres = '/usr/bin/google-chrome-stable'
+        subprocess.Popen([chromeadres, "--remote-debugging-port=12264"])
+        time.sleep(0.3)
+        driver = webdriver.Chrome(chrome_options=options)
+        driver.request_interceptor = interceptor
+        driver.get('https://www.whatismyreferer.com')
+
+        time.sleep(3)
 
     def test_chrome_can_access_requests(self):
         url = 'https://www.wikipedia.org/'
@@ -99,44 +118,17 @@ class BrowserIntegrationTest(TestCase):
         url = 'https://python.org'
         user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) ' \
                      'Chrome/28.0.1500.52 Safari/537.36 OPR/15.0.1147.100'
-        options = {
-            'proxy': {
-                'http': 'http://test:test@localhost:8081',
-                'https': 'https://test:test@localhost:8081',
-                # 'custom_authorization': 'Basic dGVzdDp0ZXN0'
-            },
-            # 'proxy': {
-            #     'http': 'socks5://192.168.1.108:1080',
-            #     'https': 'socks5://192.168.1.108:1080'
-            # },
-            # 'suppress_connection_errors': False
-        }
-        driver = webdriver.Firefox(seleniumwire_options=options)
+        driver = webdriver.Firefox()
 
         def intercept(req):
             del req.headers['User-Agent']
             req.headers['User-Agent'] = user_agent
-            # req.params = {**req.params, 'foo': 'bar'}
 
         driver.request_interceptor = intercept
 
         driver.get(url)
 
-        # import time
-        # time.sleep(60)
-
-        # for request in driver.requests:
-        #     if request.response:
-        #         print(
-        #             request.url, request.response.headers['Content-Type'], len(request.response.body)
-        #         )
-
-        # request = driver.wait_for_request(url)
-        #
-        # self.assertEqual(user_agent, request.headers['User-Agent'])
-        #
         driver.quit()
-
 
     def test_intercept_response(self):
         url = 'https://www.guardian.co.uk'
