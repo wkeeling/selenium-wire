ea45bb7bfc49b80838bdb800cacbf302e6f2320c
Will Keeling <will@zifferent.com>
2021-02-14 20:22:12 +0000

    Show stdout from tests

--- tests/utils.py
+++ tests/utils.py
@@ -6,33 +6,29 @@ from pathlib import Path
 
 
 def get_httpbin(port=8085):
-    """Get a Httpbin object containing the URL of a running
-    httpbin server.
+    """Get a Httpbin object containing the URL of a running httpbin server.
 
-    This function will attempt to discover a httpbin server by
-    trying each of the following steps in turn:
+    This function will attempt to discover a httpbin server by trying each
+    of the following steps in turn:
 
-    - Check to see if anything is listening on the given port
-    on localhost. If so, assume it's a httpbin server and
-    return a Httpbin object containing the URL.
+    - Check to see if anything is listening on the given port on localhost.
+    If so, assume it's a httpbin server and return a Httpbin object containing
+    the URL.
 
-    - Attempt to start a local httpbin server on the given
-    port and, if successful, return a Httpbin object
-    containing the URL. This will only work for non-Windows
+    - Attempt to start a local httpbin server on the given port and, if successful,
+    return a Httpbin object containing the URL. This will only work for non-Windows
     hosts.
 
-    - Return a Httpbin object containing the URL of the
-    public httpbin website, i.e. https://httpbin.org
+    - Return a Httpbin object containing the URL of the public httpbin website,
+    i.e. https://httpbin.org
 
-    Clients should call the close() method on the returned
-    Httpbin object when they are finished with it.
+    Clients should call the close() method on the returned Httpbin object when they
+    are finished with it.
 
     Args:
-        port: Optional port number that the httpbin instance
-        is/should listen on.
+        port: Optional port number that the httpbin instance is/should listen on.
     Returns:
-        A Httpbin object containing the URL of the httpbin
-        server.
+        A Httpbin object containing the URL of the httpbin server.
     """
     url = f'https://localhost:{port}'
 
