2c0239727899529e3377ff5cc0f877508386ed24
Will Keeling <will@zifferent.com>
2021-07-23 09:32:28 +0100

    Bump version: 4.3.3 → 4.4.0

--- seleniumwire/__init__.py
+++ seleniumwire/__init__.py
@@ -3,4 +3,4 @@
 """Top-level package for Selenium Wire."""
 
 __author__ = """Will Keeling"""
-__version__ = '4.3.3'
+__version__ = '4.4.0'
--- setup.py
+++ setup.py
@@ -76,6 +76,6 @@ setup(
     test_suite='tests.seleniumwire',
     tests_require=['pytest'],
     url='https://github.com/wkeeling/selenium-wire',
-    version='4.3.3',
+    version='4.4.0',
     zip_safe=False,
 )
