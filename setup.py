5a5a83c0189e0a10fbcf100d619148d6c1bc7dad
Will Keeling <will@zifferent.com>
2022-01-10 19:40:26 +0000

    Bump version: 4.5.6 → 4.6.0

--- setup.py
+++ setup.py
@@ -78,6 +78,6 @@ setup(
     test_suite='tests.seleniumwire',
     tests_require=['pytest'],
     url='https://github.com/wkeeling/selenium-wire',
-    version='4.5.6',
+    version='4.6.0',
     zip_safe=False,
 )
